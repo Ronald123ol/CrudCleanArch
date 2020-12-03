4e346673062fe33189de2478b7df9f9f9b5d150a
Ronald123ol <70978284+Ronald123ol@users.noreply.github.com>
2020-12-03 16:34:56 -0400

    Add project files.

---
+++ CrudCleanArch/Datos/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using CrudCleanArch.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudCleanArch.Datos
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+
+        {
+
+        }
+        public DbSet<Libro> Libro {get; set;}
+    }
+}
---
+++ CrudCleanArch/Models/Libro.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudCleanArch.Models
+{
+    public class Libro
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="El campo titulo es obligatorio.")]
+        [StringLength(50, ErrorMessage ="El {0} debe ser al menos {2} y máximo {1} caracteres", MinimumLength =3)]
+        [Display(Name ="Título")]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "El campo Descripcion obligatorio.")]
+        [StringLength(50, ErrorMessage = "El {0} debe ser al menos {2} y máximo {1} caracteres", MinimumLength = 3)]
+        [Display(Name = "Descrición")]
+        public string Descripcion { get; set; }
+
+        [Required(ErrorMessage = "El campo Fecha es obligatorio.")]
+        [DataType(DataType.Date)]
+        [Display(Name = "Fecha Lanzamiento")]
+        public DateTime Fecha_Lanzamiento { get; set; }
+        [Required(ErrorMessage = "El campo Autor es obligatorio.")]
+       
+        public string Autor { get; set; }
+        [Required(ErrorMessage = "El campo Precio es obligatorio.")]
+        public int Precio { get; set; }
+    }
+}
